98da18403ec2f4cd212acb3b0127d4e3b2bf6dc0
Endy Jasmi <endyjasmi@gmail.com>
2022-03-07 19:51:04 +0800

    refactor(@marubase/helper-stream): rename from isDuplex test

--- packages/helper-stream/source/is-duplex-stream.test.ts
+++ packages/helper-stream/source/is-duplex-stream.test.ts
@@ -1,18 +1,18 @@
 import { expect } from "chai";
 import { Duplex } from "stream";
-import { isDuplex } from "./is-duplex.js";
+import { isDuplexStream } from "./is-duplex-stream.js";
 
-describe("isDuplex(input)", function () {
+describe("isDuplexStream(input)", function () {
   context("when input is duplex stream", function () {
     it("should return true", async function () {
       const input = new Duplex();
-      expect(isDuplex(input)).to.be.true;
+      expect(isDuplexStream(input)).to.be.true;
     });
   });
   context("when input is not duplex stream", function () {
     it("should return false", async function () {
       const input = null;
-      expect(isDuplex(input)).to.be.false;
+      expect(isDuplexStream(input)).to.be.false;
     });
   });
 });
