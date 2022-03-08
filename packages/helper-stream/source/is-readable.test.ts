c4a1b937cdd2d534852734c3f4fd9e7dc7c7798f
Endy Jasmi <endyjasmi@gmail.com>
2022-03-08 10:22:26 +0800

    test(@marubase/helper-stream): revert to isReadable test

--- packages/helper-stream/source/is-readable.test.ts
+++ packages/helper-stream/source/is-readable.test.ts
@@ -1,18 +1,18 @@
 import { expect } from "chai";
 import { Readable } from "stream";
-import { isReadableStream } from "./is-readable-stream.js";
+import { isReadable } from "./is-readable.js";
 
-describe("isReadableStream(input)", function () {
+describe("isReadable(input)", function () {
   context("when input is readable stream", function () {
     it("should return true", async function () {
       const input = new Readable();
-      expect(isReadableStream(input)).to.be.true;
+      expect(isReadable(input)).to.be.true;
     });
   });
   context("when input is not readable stream", function () {
     it("should return false", async function () {
       const input = null;
-      expect(isReadableStream(input)).to.be.false;
+      expect(isReadable(input)).to.be.false;
     });
   });
 });
