72010b83bd652dea0431f8abbcddb796d78b7e62
Endy Jasmi <endyjasmi@gmail.com>
2022-03-08 10:22:55 +0800

    test(@marubase/helper-stream): revert to isDuplex test

--- packages/helper-stream/source/is-duplex.test.ts
+++ packages/helper-stream/source/is-duplex.test.ts
@@ -1,18 +1,18 @@
 import { expect } from "chai";
 import { Duplex } from "stream";
-import { isDuplexStream } from "./is-duplex-stream.js";
+import { isDuplex } from "./is-duplex.js";
 
-describe("isDuplexStream(input)", function () {
+describe("isDuplex(input)", function () {
   context("when input is duplex stream", function () {
     it("should return true", async function () {
       const input = new Duplex();
-      expect(isDuplexStream(input)).to.be.true;
+      expect(isDuplex(input)).to.be.true;
     });
   });
   context("when input is not duplex stream", function () {
     it("should return false", async function () {
       const input = null;
-      expect(isDuplexStream(input)).to.be.false;
+      expect(isDuplex(input)).to.be.false;
     });
   });
 });
