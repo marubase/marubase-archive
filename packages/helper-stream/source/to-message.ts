804c582d50f8c5e7c04d137a3a433134c6373963
Endy Jasmi <endyjasmi@gmail.com>
2022-03-09 13:18:17 +0800

    feat(@marubase/helper-stream): add toMessage function

---
+++ packages/helper-stream/source/to-message.ts
@@ -0,0 +1,44 @@
+import { Readable } from "stream";
+import { Message } from "./to-message-readable.js";
+
+export const MAX_HEADER_SIZE = 16384;
+
+export async function toMessage(readable: Readable): Promise<Message> {
+  const headers = new Map<string, string>();
+  const reader = readable[Symbol.asyncIterator]();
+
+  let buffer = Buffer.from([]);
+  let chunk = await reader.next();
+  for (; !chunk.done; chunk = await reader.next()) {
+    buffer = Buffer.concat([buffer, chunk.value]);
+    if (buffer.length > MAX_HEADER_SIZE) {
+      const context = `Parsing http message stream.`;
+      const problem = `No header found at the first ${MAX_HEADER_SIZE} bytes.`;
+      const solution = `Please supply a valid http message.`;
+      throw new Error(`${context} ${problem} ${solution}`);
+    }
+
+    const separator = "\r\n\r\n";
+    const separatorIndex = buffer.indexOf(separator);
+    if (separatorIndex < 0) continue;
+
+    const rawHeaders = buffer.subarray(0, separatorIndex).toString("utf8");
+    for (const rawHeader of rawHeaders.split("\r\n")) {
+      const [key, value] = rawHeader.split(":");
+      headers.set(key.trim(), value.trim());
+    }
+    buffer = buffer.subarray(separatorIndex + separator.length);
+    break;
+  }
+
+  const body = new Readable({
+    read() {
+      reader.next().then(
+        (chunk) => this.push(!chunk.done ? chunk.value : null),
+        (error) => this.destroy(error),
+      );
+    },
+  });
+  body.push(buffer);
+  return { body, headers };
+}
