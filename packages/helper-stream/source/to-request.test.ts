cd658eb9d6181cefff8e34e4a34be9e1b8c92470
Endy Jasmi <endyjasmi@gmail.com>
2022-03-09 20:48:02 +0800

    test(@marubase/helper-stream): add toRequest test

---
+++ packages/helper-stream/source/to-request.test.ts
@@ -0,0 +1,87 @@
+import { expect } from "chai";
+import { Readable } from "stream";
+import { isReadable } from "./is-readable.js";
+import { toBufferReadable } from "./to-buffer-readable.js";
+import { toJsonData } from "./to-json-data.js";
+import { toRequestReadable } from "./to-request-readable.js";
+import { toRequest } from "./to-request.js";
+import { toTextData } from "./to-text-data.js";
+
+describe("toRequest(readable)", function () {
+  context("when readable is a valid message stream", function () {
+    it("should return message", async function () {
+      let rawContent = "GET /test HTTP/1.1\r\n";
+      for (let i = 0; i < 818; i++) {
+        const index = i.toString(10).padStart(5);
+        rawContent += `Test-Header: ${index}\r\n`;
+      }
+      rawContent += `\r\n`;
+      rawContent += `{"test":true}`;
+
+      const readable = toBufferReadable(rawContent);
+      const request = await toRequest(readable);
+      const { headers, body } = request;
+      expect(headers).to.be.instanceOf(Map);
+      expect(isReadable(body)).to.be.true;
+
+      const { method, path, protocol } = request;
+      expect(method).to.equal("GET");
+      expect(path).to.equal("/test");
+      expect(protocol).to.equal("HTTP/1.1");
+
+      const data = await toJsonData(body);
+      expect(data).to.deep.equal({ test: true });
+    });
+  });
+  context("when readable is not a valid message stream #1", function () {
+    it("should return message", async function () {
+      let rawContent = "GET /test HTTP/1.1\r\n";
+      for (let i = 0; i < 820; i++) {
+        const index = i.toString(10).padStart(5);
+        rawContent += `Test-Header: ${index}\r\n`;
+      }
+      rawContent += `\r\n`;
+      rawContent += `{"test":true}`;
+
+      try {
+        const readable = toBufferReadable(rawContent);
+        await toRequest(readable);
+      } catch (error) {} /* eslint-disable-line no-empty */
+    });
+  });
+  context("when readable is not a valid message stream #2", function () {
+    it("should return message", async function () {
+      let rawContent = "";
+      for (let i = 0; i < 5; i++) {
+        const index = i.toString(10).padStart(5);
+        rawContent += `Test-Header: ${index}\r\n`;
+      }
+      rawContent += `\r\n`;
+      rawContent += `{"test":true}`;
+
+      try {
+        const readable = toBufferReadable(rawContent);
+        await toRequest(readable);
+      } catch (error) {} /* eslint-disable-line no-empty */
+    });
+  });
+  context("when readable throw error", function () {
+    it("should return message", async function () {
+      const headers = new Map([
+        ["Content-Type", "application/json"],
+        ["Content-Length", "14"],
+      ]);
+      const body = new Readable({
+        read() {
+          this.destroy(new Error("test error"));
+        },
+      });
+
+      try {
+        const readable = toRequestReadable({ body, headers });
+        const { body: content } = await toRequest(readable);
+        await toTextData(content);
+      } catch (error) {} /* eslint-disable-line no-empty */
+    });
+  });
+});
