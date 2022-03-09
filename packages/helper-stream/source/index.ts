a64927b40ceb065e39fb372e997fe693caa7335d
Endy Jasmi <endyjasmi@gmail.com>
2022-03-09 14:20:40 +0800

    refactor(@marubase/helper-stream): update package index

--- packages/helper-stream/source/index.ts
+++ packages/helper-stream/source/index.ts
@@ -8,4 +8,5 @@ export * from "./to-collection.js";
 export * from "./to-json-data.js";
 export * from "./to-message-readable.js";
 export * from "./to-message.js";
+export * from "./to-multipart-readable.js";
 export * from "./to-text-data.js";
