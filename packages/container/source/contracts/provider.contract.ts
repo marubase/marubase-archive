e931571b1ce2b676cdf960ffc70440487894d4fd
Endy Jasmi <endyjasmi@gmail.com>
2022-03-16 13:15:16 +0800

    feat(@marubase/container): add ProviderInterface contract

---
+++ packages/container/source/contracts/provider.contract.ts
@@ -0,0 +1,17 @@
+import { ContainerInterface } from "./container.contract.js";
+
+export const ProviderContract = Symbol("ProviderContract");
+
+export interface ProviderInterface {
+  boot?(container: ContainerInterface): Promise<void>;
+
+  install?(container: ContainerInterface): void;
+
+  shutdown?(container: ContainerInterface): Promise<void>;
+
+  uninstall?(container: ContainerInterface): void;
+}
+
+export type ProviderMap = Map<ProviderName, ProviderInterface>;
+
+export type ProviderName = string | symbol;
