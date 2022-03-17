39ad3bf126ed96ede39881a82209c67129292686
Endy Jasmi <endyjasmi@gmail.com>
2022-03-17 15:31:33 +0800

    test(@marubase/container): add test index

---
+++ packages/container/source/index.test.ts
@@ -0,0 +1,6 @@
+import "./cache.test.js";
+import "./container.test.js";
+import "./registry-binding.test.js";
+import "./registry.test.js";
+import "./resolvers/index.test.js";
+import "./scope.test.js";
