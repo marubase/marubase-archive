a943e0f762243a94b295e2165ca57420098a11ea
Endy Jasmi <endyjasmi@gmail.com>
2022-03-17 13:13:36 +0800

    feat(@marubase/container): add function resolver implementation

---
+++ packages/container/source/resolvers/function-resolver.ts
@@ -0,0 +1,25 @@
+import {
+  RegistryInterface,
+  ResolvableFunction,
+} from "../contracts/registry.contract.js";
+import { ResolverInterface } from "../contracts/resolver.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
+import { BaseResolver } from "./base-resolver.js";
+
+export class FunctionResolver
+  extends BaseResolver
+  implements ResolverInterface
+{
+  protected _target: Function;
+
+  public constructor(registry: RegistryInterface, target: Function) {
+    super(registry);
+    this._target = target;
+  }
+
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
+    const target = this._target as ResolvableFunction<Result>;
+    const targetArgs = this._resolveDependencies(scope).concat(...args);
+    return target(...targetArgs);
+  }
+}
