96707ee917d0a4cc3b226605c1e62b721ea058fd
Endy Jasmi <endyjasmi@gmail.com>
2022-03-17 13:16:55 +0800

    feat(@marubase/container): add package index

---
+++ packages/container/source/index.ts
@@ -0,0 +1,7 @@
+export * from "./cache.js";
+export * from "./container.js";
+export * from "./contracts/index.js";
+export * from "./errors/index.js";
+export * from "./registry.js";
+export * from "./resolvers/index.js";
+export * from "./scope.js";
---
+++ packages/container/source/resolvers/index.ts
@@ -0,0 +1,7 @@
+export * from "./base-resolver.js";
+export * from "./binding-resolver.js";
+export * from "./callable-resolver.js";
+export * from "./class-resolver.js";
+export * from "./constant-resolver.js";
+export * from "./function-resolver.js";
+export * from "./tag-resolver.js";
