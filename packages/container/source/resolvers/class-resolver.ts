44273c4ae72e58c5f35da266acca6308d85a28c9
Endy Jasmi <endyjasmi@gmail.com>
2022-03-18 11:13:00 +0800

    refactor(@marubase/container): update class resolver implementation

--- packages/container/source/resolvers/class-resolver.ts
+++ packages/container/source/resolvers/class-resolver.ts
@@ -4,6 +4,10 @@ import {
 } from "../contracts/registry.contract.js";
 import { ResolverInterface } from "../contracts/resolver.contract.js";
 import { ScopeInterface } from "../contracts/scope.contract.js";
+import { getDependencies } from "../functions/get-dependencies.js";
+import { getScope } from "../functions/get-scope.js";
+import { getTags } from "../functions/get-tags.js";
+import { isInjectable } from "../functions/is-injectable.js";
 import { BaseResolver } from "./base-resolver.js";
 
 export class ClassResolver extends BaseResolver implements ResolverInterface {
@@ -12,6 +16,17 @@ export class ClassResolver extends BaseResolver implements ResolverInterface {
   public constructor(registry: RegistryInterface, target: Function) {
     super(registry);
     this._target = target;
+
+    if (isInjectable(this._target)) {
+      const resolverDependencies = getDependencies(this._target);
+      this.setDependencies(...resolverDependencies);
+
+      const resolverScope = getScope(this._target);
+      this.setScope(resolverScope);
+
+      const resolverTags = getTags(this._target);
+      this.setTags(...resolverTags);
+    }
   }
 
   public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
