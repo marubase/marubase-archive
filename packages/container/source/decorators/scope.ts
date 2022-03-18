934069f3dadba991dd7f85e5ac1674b656bc5586
Endy Jasmi <endyjasmi@gmail.com>
2022-03-18 11:12:14 +0800

    feat(@marubase/container): add scope decorator

---
+++ packages/container/source/decorators/scope.ts
@@ -0,0 +1,8 @@
+import { ResolverScope } from "../contracts/resolver.contract.js";
+import { setScope } from "../functions/set-scope.js";
+
+export function scope(scope: ResolverScope): ClassDecorator & MethodDecorator {
+  return (target: Function | Object, propertyKey?: string | symbol) => {
+    setScope(target, propertyKey, () => scope);
+  };
+}
