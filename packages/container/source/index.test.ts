379202a3c55a1e9184aa5b0ab3ad0cb92e80ab6a
Endy Jasmi <endyjasmi@gmail.com>
2022-03-18 11:59:09 +0800

    test(@marubase/container): clear tests

--- packages/container/source/index.test.ts
+++
@@ -1,7 +0,0 @@
-import "reflect-metadata";
-import "./cache.test.js";
-import "./container.test.js";
-import "./registry-binding.test.js";
-import "./registry.test.js";
-import "./resolvers/index.test.js";
-import "./scope.test.js";
--- packages/container/source/resolvers/index.test.ts
+++
@@ -1,7 +0,0 @@
-import "./base-resolver.test.js";
-import "./binding-resolver.test.js";
-import "./callable-resolver.test.js";
-import "./class-resolver.test.js";
-import "./constant-resolver.test.js";
-import "./function-resolver.test.js";
-import "./tag-resolver.test.js";
