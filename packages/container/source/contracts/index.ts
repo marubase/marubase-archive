b84da030bd103f852eba8ee46cbbf284a5f2dd2e
Endy Jasmi <endyjasmi@gmail.com>
2022-03-19 16:41:03 +0800

    feat(@marubase/container): add package index

---
+++ packages/container/source/contracts/index.ts
@@ -0,0 +1,5 @@
+export * from "./cache.contract.js";
+export * from "./container.contract.js";
+export * from "./registry.contract.js";
+export * from "./resolver.contract.js";
+export * from "./scope.contract.js";
---
+++ packages/container/source/index.ts
@@ -0,0 +1,3 @@
+import "reflect-metadata";
+export * from "./contracts/index.js";
+export * from "./errors/index.js";
