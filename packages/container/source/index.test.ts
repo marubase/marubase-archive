4073f878cefb676b3bb98df45818b4c3b7f74c74
Endy Jasmi <endyjasmi@gmail.com>
2022-03-19 20:17:04 +0800

    test(@marubase/container): add test index

---
+++ packages/container/source/index.test.ts
@@ -0,0 +1,2 @@
+import "reflect-metadata";
+import "./resolvers/index.test.js";
---
+++ packages/container/source/resolvers/index.test.ts
@@ -0,0 +1,4 @@
+import "./base-resolver.test.js";
+import "./class-resolver.test.js";
+import "./constant-resolver.test.js";
+import "./registry-key-resolver.test.js";
