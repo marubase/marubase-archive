26d406b030f9bc1df0a6a1c6e19227f27969af39
Endy Jasmi <endyjasmi@gmail.com>
2022-03-19 20:16:31 +0800

    feat(@marubase/container): add constant resolver implementation

---
+++ packages/container/source/resolvers/constant-resolver.ts
@@ -0,0 +1,18 @@
+import { RegistryContract } from "../contracts/registry.contract.js";
+import { ResolverContract } from "../contracts/resolver.contract.js";
+import { ScopeContract } from "../contracts/scope.contract.js";
+import { BaseResolver } from "./base-resolver.js";
+
+export class ConstantResolver extends BaseResolver implements ResolverContract {
+  protected _constant: unknown;
+
+  public constructor(registry: RegistryContract, constant: unknown) {
+    super(registry);
+    this._constant = constant;
+  }
+
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+    return this._constant as Result;
+  }
+}
