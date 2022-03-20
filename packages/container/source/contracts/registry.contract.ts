d6c8ac380b592069af17b28e18c962ca4bcceced
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 17:16:07 +0800

    refactor(@marubase/container): update registry contract

--- packages/container/source/contracts/registry.contract.ts
+++ packages/container/source/contracts/registry.contract.ts
@@ -11,11 +11,20 @@ export interface RegistryContract {
 
   readonly resolverFactory: ResolverFactory;
 
+  readonly tagMap: Map<RegistryTag, Set<ResolverContract>>;
+
   bind(key: RegistryKey): RegistryBindTo;
 
   clearResolverByKey(key: RegistryKey): this;
 
+  clearResolverByTags(
+    tagSet: Set<RegistryTag>,
+    resolver: ResolverContract,
+  ): this;
+
   getResolverByKey(key: RegistryKey): ResolverContract | undefined;
+
+  getResolverByTag(tag: RegistryTag): ResolverContract[];
 
   resolve<Result>(
     resolvable: Resolvable,
@@ -23,7 +32,15 @@ export interface RegistryContract {
     ...args: unknown[]
   ): Result;
 
+  resolveTag<Result>(
+    tag: RegistryTag,
+    scope: ScopeContract,
+    ...args: unknown[]
+  ): Result[];
+
   setResolverByKey(key: RegistryKey, resolver: ResolverContract): this;
+
+  setResolverByTags(tagSet: Set<RegistryTag>, resolver: ResolverContract): this;
 }
 
 export type RegistryBindTo = {
@@ -38,6 +55,10 @@ export type RegistryBindTo = {
   toFunction(targetFn: Function): ResolverContract;
 
   toSelf(): ResolverContract;
+
+  toTag(targetTag: RegistryTag): ResolverContract;
 };
 
 export type RegistryKey = Function | string | symbol;
+
+export type RegistryTag = string | symbol;
