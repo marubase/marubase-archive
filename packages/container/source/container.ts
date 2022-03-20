163b6710d805045f9e0522a15d248554ae7cf26c
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 20:16:03 +0800

    refactor(@marubase/container): update container implementation

--- packages/container/source/container.ts
+++ packages/container/source/container.ts
@@ -1,4 +1,8 @@
-import { ContainerContract } from "./contracts/container.contract.js";
+import {
+  ContainerContract,
+  Provider,
+  ProviderName,
+} from "./contracts/container.contract.js";
 import {
   RegistryBindTo,
   RegistryContract,
@@ -11,6 +15,10 @@ import { Registry } from "./registry.js";
 import { Scope } from "./scope.js";
 
 export class Container implements ContainerContract {
+  protected _booted = false;
+
+  protected _providerMap = new Map<ProviderName, Provider>();
+
   protected _registry: RegistryContract;
 
   protected _scope: ScopeContract;
@@ -19,6 +27,14 @@ export class Container implements ContainerContract {
     this._registry = registry || new Registry();
     this._scope = scope || new Scope();
     this.bind("Container").toConstant(this);
+  }
+
+  public get booted(): boolean {
+    return this._booted;
+  }
+
+  public get providerMap(): Map<ProviderName, Provider> {
+    return this._providerMap;
   }
 
   public get registry(): RegistryContract {
@@ -33,10 +49,31 @@ export class Container implements ContainerContract {
     return this._registry.bind(key);
   }
 
+  public async boot(): Promise<void> {
+    if (this._booted) return;
+
+    const bootPromises: Promise<void>[] = [];
+    for (const [, provider] of this._providerMap)
+      if (provider.boot) bootPromises.push(provider.boot(this));
+    await Promise.all(bootPromises);
+    this._booted = true;
+  }
+
   public fork(): this {
     const Static = this.constructor as typeof Container;
     const containerScope = this._scope.fork("container");
     return new Static(this._registry, containerScope) as this;
+  }
+
+  public install(name: ProviderName, provider: Provider): this {
+    if (provider.install) provider.install(this);
+    if (this._booted && provider.boot) provider.boot(this);
+    this._providerMap.set(name, provider);
+    return this;
+  }
+
+  public installed(name: ProviderName): boolean {
+    return this._providerMap.has(name);
   }
 
   public resolve<Result>(resolvable: RegistryKey, ...args: unknown[]): Result {
@@ -51,5 +88,29 @@ export class Container implements ContainerContract {
 
   public resolver(key: RegistryKey): ResolverContract | undefined {
     return this._registry.getResolverByKey(key);
+  }
+
+  public async shutdown(): Promise<void> {
+    if (!this._booted) return;
+
+    const shutdownPromises: Promise<void>[] = [];
+    for (const [, provider] of this._providerMap)
+      if (provider.shutdown) shutdownPromises.push(provider.shutdown(this));
+    await Promise.all(shutdownPromises);
+    this._booted = false;
+  }
+
+  public uninstall(name: ProviderName): this {
+    const provider = this._providerMap.get(name);
+    if (typeof provider === "undefined") return this;
+
+    if (this._booted && provider.shutdown)
+      provider.shutdown(this).then(() => {
+        return provider.uninstall
+          ? provider.uninstall(this)
+          : Promise.resolve();
+      });
+    else if (provider.uninstall) provider.uninstall(this);
+    return this;
   }
 }
