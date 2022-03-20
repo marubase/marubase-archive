729df89643b3ac5a9054b17006681bf6288def07
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 16:55:28 +0800

    refactor(@marubase/container): update package index

--- packages/container/source/resolvers/index.ts
+++ packages/container/source/resolvers/index.ts
@@ -2,4 +2,5 @@ export * from "./base-resolver.js";
 export * from "./callable-resolver.js";
 export * from "./class-resolver.js";
 export * from "./constant-resolver.js";
+export * from "./function-resolver.js";
 export * from "./registry-key-resolver.js";
