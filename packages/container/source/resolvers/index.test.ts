b4075fac37bd2c8efa458caa0e1af0d21b1f5bd0
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 16:55:19 +0800

    test(@marubase/container): update test index

--- packages/container/source/resolvers/index.test.ts
+++ packages/container/source/resolvers/index.test.ts
@@ -2,4 +2,5 @@ import "./base-resolver.test.js";
 import "./callable-resolver.test.js";
 import "./class-resolver.test.js";
 import "./constant-resolver.test.js";
+import "./function-resolver.test.js";
 import "./registry-key-resolver.test.js";
