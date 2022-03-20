a228f7205c6f41b6b6bd94df51ebfe52ac152dd0
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 18:05:43 +0800

    refactor(@marubase/container): update container contract

--- packages/container/source/contracts/container.contract.ts
+++ packages/container/source/contracts/container.contract.ts
@@ -8,17 +8,41 @@ import { Resolvable, ResolverContract } from "./resolver.contract.js";
 import { ScopeContract } from "./scope.contract.js";
 
 export interface ContainerContract {
+  readonly booted: boolean;
+
+  readonly providerMap: Map<ProviderName, Provider>;
+
   readonly registry: RegistryContract;
 
   readonly scope: ScopeContract;
 
   bind(key: RegistryKey): RegistryBindTo;
 
+  boot(): Promise<void>;
+
   fork(): this;
+
+  install(name: ProviderName, provider: Provider): this;
 
   resolve<Result>(resolvable: Resolvable, ...args: unknown[]): Result;
 
   resolveTag<Result>(tag: RegistryTag, ...args: unknown[]): Result[];
 
   resolver(key: RegistryKey): ResolverContract | undefined;
+
+  shutdown(): Promise<void>;
+
+  uninstall(name: ProviderName): this;
 }
+
+export type Provider = {
+  boot?(container: ContainerContract): Promise<void>;
+
+  install?(container: ContainerContract): void;
+
+  shutdown?(container: ContainerContract): Promise<void>;
+
+  uninstall?(container: ContainerContract): void;
+};
+
+export type ProviderName = string | symbol;
