7426f24f26564fe968a05f0ee8bd1f61ad951681
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 15:49:54 +0800

    feat(@marubase/container): update package index

--- packages/container/source/index.ts
+++ packages/container/source/index.ts
@@ -2,5 +2,6 @@ import "reflect-metadata";
 export * from "./cache.js";
 export * from "./contracts/index.js";
 export * from "./errors/index.js";
+export * from "./registry.js";
 export * from "./resolvers/index.js";
 export * from "./scope.js";
