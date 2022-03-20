f22f6b2c10a854bf2f0d4a127ba5f8f5eb2f2f74
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 16:54:25 +0800

    test(@marubase/container): add function resolver test

---
+++ packages/container/source/resolvers/function-resolver.test.ts
@@ -0,0 +1,44 @@
+import { expect } from "chai";
+import { instance, mock, when } from "ts-mockito";
+import { RegistryContract } from "../contracts/registry.contract.js";
+import { ScopeContract } from "../contracts/scope.contract.js";
+import { FunctionResolver } from "./function-resolver.js";
+
+describe("FunctionResolver", function () {
+  let mockRegistry: RegistryContract;
+  let mockScope: ScopeContract;
+  let instanceRegistry: RegistryContract;
+  let instanceScope: ScopeContract;
+  beforeEach(async function () {
+    mockRegistry = mock();
+    mockScope = mock();
+    instanceRegistry = instance(mockRegistry);
+    instanceScope = instance(mockScope);
+  });
+
+  describe("#resolve(scope, ...args)", function () {
+    context("when there is dependencies", function () {
+      let resolver: FunctionResolver;
+      beforeEach(async function () {
+        resolver = new FunctionResolver(instanceRegistry, (format: string) => new Date(format));
+        resolver.setDependencies("date");
+
+        when(mockRegistry.resolve<string>("date", instanceScope)).thenReturn("1990-09-03");
+      });
+      it("should return result", async function () {
+        const returnResult = resolver.resolve(instanceScope);
+        expect(returnResult).to.be.an.instanceOf(Date);
+      });
+    });
+    context("when there is no dependencies", function () {
+      let resolver: FunctionResolver;
+      beforeEach(async function () {
+        resolver = new FunctionResolver(instanceRegistry, (format: string) => new Date(format));
+      });
+      it("should return result", async function () {
+        const returnResult = resolver.resolve(instanceScope);
+        expect(returnResult).to.be.an.instanceOf(Date);
+      });
+    });
+  });
+});
