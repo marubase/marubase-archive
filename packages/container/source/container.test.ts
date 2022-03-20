2532a878dae0bf950702a79e3c4aff2ff51c5ef9
Endy Jasmi <endyjasmi@gmail.com>
2022-03-20 16:08:52 +0800

    test(@marubase/container): add container test

---
+++ packages/container/source/container.test.ts
@@ -0,0 +1,80 @@
+import { expect } from "chai";
+import { Container } from "./container.js";
+import { ContainerError } from "./errors/container.error.js";
+import { Registry } from "./registry.js";
+import { BaseResolver } from "./resolvers/base-resolver.js";
+import { Scope } from "./scope.js";
+
+describe("Container", function () {
+  let container: Container;
+  beforeEach(async function () {
+    container = new Container();
+  });
+
+  describe("get registry", function () {
+    it("should return registry", async function () {
+      const returnRegistry = container.registry;
+      expect(returnRegistry).to.be.an.instanceOf(Registry);
+    });
+  });
+
+  describe("get scope", function () {
+    it("should return scope", async function () {
+      const returnScope = container.scope;
+      expect(returnScope).to.be.an.instanceOf(Scope);
+    });
+  });
+
+  describe("#bind(key)", function () {
+    it("should return registry binding fluent api", async function () {
+      const returnFluentApi = container.bind("test");
+      expect(returnFluentApi).to.have.property("to");
+      expect(returnFluentApi).to.have.property("toAlias");
+      expect(returnFluentApi).to.have.property("toConstant");
+      expect(returnFluentApi).to.have.property("toSelf");
+    });
+  });
+
+  describe("#fork()", function () {
+    it("should return fork", async function () {
+      const returnFork = container.fork();
+      expect(returnFork).to.be.an.instanceOf(Container);
+    });
+  });
+
+  describe("#resolve(resolvable, ...args)", function () {
+    context("when there is record", function () {
+      beforeEach(async function () {
+        container.bind(Date).toSelf();
+      });
+      it("should return resolved instance", async function () {
+        const returnInstance = container.resolve(Date);
+        expect(returnInstance).to.be.an.instanceOf(Date);
+      });
+    });
+    context("when there is no record", function () {
+      it("should throw error", async function () {
+        const run = (): unknown => container.resolve(Date);
+        expect(run).to.throw(ContainerError);
+      });
+    });
+  });
+
+  describe("#resolver(key)", function () {
+    context("when there is record", function () {
+      beforeEach(async function () {
+        container.bind(Date).toSelf();
+      });
+      it("should return resolver", async function () {
+        const returnResolver = container.resolver(Date);
+        expect(returnResolver).to.be.an.instanceOf(BaseResolver);
+      });
+    });
+    context("when there is no record", function () {
+      it("should return undefined", async function () {
+        const returnResolver = container.resolver(Date);
+        expect(returnResolver).to.be.undefined;
+      });
+    });
+  });
+});
