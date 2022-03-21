5f35e0dd2b6fce4cfc94ebadad05d1b75c11a18c
Endy Jasmi <endyjasmi@gmail.com>
2022-03-21 08:38:31 +0800

    test(@marubase/container): update function resolver test

--- packages/container/source/resolvers/function-resolver.test.ts
+++ packages/container/source/resolvers/function-resolver.test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { instance, mock, when } from "ts-mockito";
 import { RegistryContract } from "../contracts/registry.contract.js";
 import { ScopeContract } from "../contracts/scope.contract.js";
+import { setInjectable } from "../functions/set-injectable.js";
 import { FunctionResolver } from "./function-resolver.js";
 
 describe("FunctionResolver", function () {
@@ -34,6 +35,22 @@ describe("FunctionResolver", function () {
       let resolver: FunctionResolver;
       beforeEach(async function () {
         resolver = new FunctionResolver(instanceRegistry, (format: string) => new Date(format));
+      });
+      it("should return result", async function () {
+        const returnResult = resolver.resolve(instanceScope);
+        expect(returnResult).to.be.an.instanceOf(Date);
+      });
+    });
+    context("when target is injectable", function () {
+      let resolver: FunctionResolver;
+      beforeEach(async function () {
+        const targetFn = (format: string): Date => new Date(format);
+        setInjectable(true, targetFn);
+
+        resolver = new FunctionResolver(instanceRegistry, targetFn);
+        resolver.setDependencies("date");
+
+        when(mockRegistry.resolve<string>("date", instanceScope)).thenReturn("1990-09-03");
       });
       it("should return result", async function () {
         const returnResult = resolver.resolve(instanceScope);
