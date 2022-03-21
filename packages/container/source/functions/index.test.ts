69b1fa803b35533054e9f7a330b4c50e7f9b3638
Endy Jasmi <endyjasmi@gmail.com>
2022-03-21 08:39:17 +0800

    test(@marubase/container): update test index

---
+++ packages/container/source/functions/index.test.ts
@@ -0,0 +1,9 @@
+import "./get-dependencies.test.js";
+import "./get-injections.test.js";
+import "./get-scope.test.js";
+import "./get-tags.test.js";
+import "./is-injectable.test.js";
+import "./set-injectable.test.js";
+import "./set-injections.test.js";
+import "./set-scope.test.js";
+import "./set-tags.test.js";
--- packages/container/source/index.test.ts
+++ packages/container/source/index.test.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import "./cache.test.js";
 import "./container.test.js";
+import "./decorators/index.test.js";
+import "./functions/index.test.js";
 import "./registry.test.js";
 import "./resolvers/index.test.js";
 import "./scope.test.js";
