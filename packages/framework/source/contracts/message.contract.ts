0ee1f02cd26030156f7100c59fa25ba849cd518e
Endy Jasmi <endyjasmi@gmail.com>
2022-03-21 10:20:15 +0800

    feat(@marubase/framework): add message contract

---
+++ packages/framework/source/contracts/message.contract.ts
@@ -0,0 +1,40 @@
+import { Readable } from "stream";
+import { MultipartContract } from "./multipart.contract.js";
+
+export interface MessageContract {
+  readonly body: Readable;
+
+  readonly headerMap: Map<string, string>;
+
+  clearBody(): this;
+
+  clearHeader(key: string): this;
+
+  clearHeaders(): this;
+
+  setBody(buffer: MessageBuffer): this;
+  setBody(data: MessageData): this;
+  setBody(multipart: MultipartContract): this;
+  setBody(stream: Readable): this;
+
+  setHeader(key: string, value: string): this;
+
+  setHeaders(headers: Map<string, string>): this;
+  setHeaders(headers: [string, string][]): this;
+
+  toBuffer(): Promise<Buffer>;
+
+  toData(): Promise<MessageData>;
+
+  toMultipart(): AsyncIterable<MessageContract>;
+}
+
+export type MessageBuffer = ArrayBuffer | NodeJS.TypedArray;
+
+export type MessageData =
+  | { [property: string]: MessageData }
+  | MessageData[]
+  | boolean
+  | number
+  | string
+  | undefined;
