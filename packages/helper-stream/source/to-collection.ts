7e32856896f1f338e834ba9306d9031a8737e7af
Endy Jasmi <endyjasmi@gmail.com>
2022-03-22 19:51:34 +0800

    chore: remove @marubase/helper-stream package

--- packages/helper-stream/source/to-collection.ts
+++
@@ -1,7 +0,0 @@
-import { Readable } from "stream";
-
-export async function toCollection<T>(readable: Readable): Promise<Array<T>> {
-  const collection: T[] = [];
-  for await (const item of readable) collection.push(item);
-  return collection;
-}
