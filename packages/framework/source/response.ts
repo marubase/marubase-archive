a7afb154e810b90c6979c5ceb4f682d29702168d
Endy Jasmi <endyjasmi@gmail.com>
2022-03-27 17:01:49 +0800

    feat(@marubase/framework): add response class

---
+++ packages/framework/source/response.ts
@@ -0,0 +1,48 @@
+import { Readable } from "stream";
+import { MessageProtocol } from "./contracts/message.contract.js";
+import {
+  ResponseCode,
+  ResponseContract,
+  ResponseText,
+} from "./contracts/response.contract.js";
+import { toResponseReadable } from "./functions/to-response-readable.js";
+import { toResponse } from "./functions/to-response.js";
+import { Message } from "./message.js";
+
+export class Response extends Message implements ResponseContract {
+  public static async from(readable: Readable): Promise<Response> {
+    const rawResponse = await toResponse(readable);
+    return new Response()
+      .setProtocol(rawResponse.protocol as MessageProtocol)
+      .setStatusCode(rawResponse.statusCode as ResponseCode)
+      .setStatusText(rawResponse.statusText)
+      .setHeaders(rawResponse.headers)
+      .setBody(rawResponse.body);
+  }
+
+  protected _statusCode: ResponseCode = 200;
+
+  protected _statusText: string = ResponseText[200];
+
+  public get statusCode(): ResponseCode {
+    return this._statusCode;
+  }
+
+  public get statusText(): string {
+    return this._statusText;
+  }
+
+  public setStatusCode(statusCode: ResponseCode): this {
+    this._statusCode = statusCode;
+    return this;
+  }
+
+  public setStatusText(statusText: string): this {
+    this._statusText = statusText;
+    return this;
+  }
+
+  public toStream(): Readable {
+    return toResponseReadable(this);
+  }
+}
