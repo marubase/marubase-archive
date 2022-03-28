02b87236eb277ce3a33bd26d52dbfed18434bccd
Endy Jasmi <endyjasmi@gmail.com>
2022-03-28 21:27:20 +0800

    refactor(@marubase/container): update CallableResolver class

--- packages/container/source/resolvers/callable-resolver.ts
+++ packages/container/source/resolvers/callable-resolver.ts
@@ -1,28 +1,31 @@
 import {
-  RegistryContract,
+  RegistryInterface,
   RegistryKey,
 } from "../contracts/registry.contract.js";
 import {
   Callable,
   ResolvableInstance,
-  ResolverContract,
+  ResolverInterface,
 } from "../contracts/resolver.contract.js";
-import { ScopeContract } from "../contracts/scope.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
 import { getDependencies } from "../functions/get-dependencies.js";
 import { getScope } from "../functions/get-scope.js";
 import { getTags } from "../functions/get-tags.js";
 import { isInjectable } from "../functions/is-injectable.js";
 import { BaseResolver } from "./base-resolver.js";
 
-export class CallableResolver extends BaseResolver implements ResolverContract {
+export class CallableResolver
+  extends BaseResolver
+  implements ResolverInterface
+{
   protected _callable: Callable;
 
-  public constructor(registry: RegistryContract, callable: Callable) {
+  public constructor(registry: RegistryInterface, callable: Callable) {
     super(registry);
     this._callable = callable;
   }
 
-  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
     const [targetKey, targetMethod] = this._callable;
     const target = this._registry.resolve(targetKey, scope);
     const targetInstance = target as ResolvableInstance<Result>;
@@ -41,7 +44,7 @@ export class CallableResolver extends BaseResolver implements ResolverContract {
     return targetInstance[targetMethod](...targetArgs);
   }
 
-  protected _resolveDependencies(scope: ScopeContract): unknown[] {
+  protected _resolveDependencies(scope: ScopeInterface): unknown[] {
     const toInstance = (dependency: RegistryKey): unknown =>
       this._registry.resolve(dependency, scope);
     return this._dependencies.map(toInstance);
