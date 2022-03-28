babda0e5d120706c16f15306b83595988b9ce656
Endy Jasmi <endyjasmi@gmail.com>
2022-03-28 21:28:26 +0800

    refactor(@marubase/container): update FunctionResolver class

--- packages/container/source/resolvers/function-resolver.ts
+++ packages/container/source/resolvers/function-resolver.ts
@@ -1,22 +1,25 @@
 import {
-  RegistryContract,
+  RegistryInterface,
   RegistryKey,
 } from "../contracts/registry.contract.js";
 import {
   ResolvableFunction,
-  ResolverContract,
+  ResolverInterface,
 } from "../contracts/resolver.contract.js";
-import { ScopeContract } from "../contracts/scope.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
 import { getDependencies } from "../functions/get-dependencies.js";
 import { getScope } from "../functions/get-scope.js";
 import { getTags } from "../functions/get-tags.js";
 import { isInjectable } from "../functions/is-injectable.js";
 import { BaseResolver } from "./base-resolver.js";
 
-export class FunctionResolver extends BaseResolver implements ResolverContract {
+export class FunctionResolver
+  extends BaseResolver
+  implements ResolverInterface
+{
   protected _targetFn: Function;
 
-  public constructor(registry: RegistryContract, targetFn: Function) {
+  public constructor(registry: RegistryInterface, targetFn: Function) {
     super(registry);
     this._targetFn = targetFn;
 
@@ -32,13 +35,13 @@ export class FunctionResolver extends BaseResolver implements ResolverContract {
     }
   }
 
-  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
     const targetFn = this._targetFn as ResolvableFunction<Result>;
     const targetArgs = this._resolveDependencies(scope).concat(...args);
     return targetFn(...targetArgs);
   }
 
-  protected _resolveDependencies(scope: ScopeContract): unknown[] {
+  protected _resolveDependencies(scope: ScopeInterface): unknown[] {
     const toInstance = (dependency: RegistryKey): unknown =>
       this._registry.resolve(dependency, scope);
     return this._dependencies.map(toInstance);
