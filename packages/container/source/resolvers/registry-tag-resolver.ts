21bc381444f58002d07b62d952c8493aab6bd15f
Endy Jasmi <endyjasmi@gmail.com>
2022-03-28 21:29:09 +0800

    refactor(@marubase/container): update RegistryTagResolver class

--- packages/container/source/resolvers/registry-tag-resolver.ts
+++ packages/container/source/resolvers/registry-tag-resolver.ts
@@ -1,26 +1,26 @@
 import {
-  RegistryContract,
+  RegistryInterface,
   RegistryTag,
 } from "../contracts/registry.contract.js";
-import { ResolverContract } from "../contracts/resolver.contract.js";
-import { ScopeContract } from "../contracts/scope.contract.js";
+import { ResolverInterface } from "../contracts/resolver.contract.js";
+import { ScopeInterface } from "../contracts/scope.contract.js";
 import { BaseResolver } from "./base-resolver.js";
 
 export class RegistryTagResolver
   extends BaseResolver
-  implements ResolverContract
+  implements ResolverInterface
 {
   protected _targetTag: RegistryTag;
 
-  public constructor(registry: RegistryContract, targetTag: RegistryTag) {
+  public constructor(registry: RegistryInterface, targetTag: RegistryTag) {
     super(registry);
     this._targetTag = targetTag;
   }
 
-  public resolve<Result>(scope: ScopeContract, ...args: unknown[]): Result {
+  public resolve<Result>(scope: ScopeInterface, ...args: unknown[]): Result {
     const resolvers = this._registry.getResolverByTag(this._targetTag);
 
-    const toResolved = (resolver: ResolverContract): unknown =>
+    const toResolved = (resolver: ResolverInterface): unknown =>
       resolver.resolve(scope, ...args);
     return resolvers.map(toResolved) as unknown as Result;
   }
