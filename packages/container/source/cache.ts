9039a64a8858016917d2b59370c95b18ea6d1ae5
Endy Jasmi <endyjasmi@gmail.com>
2022-03-28 21:29:21 +0800

    refactor(@marubase/container): update Cache class

--- packages/container/source/cache.ts
+++ packages/container/source/cache.ts
@@ -1,7 +1,7 @@
-import { CacheContract } from "./contracts/cache.contract.js";
+import { CacheInterface } from "./contracts/cache.contract.js";
 import { RegistryKey } from "./contracts/registry.contract.js";
 
-export class Cache extends Map<RegistryKey, unknown> implements CacheContract {
+export class Cache extends Map<RegistryKey, unknown> implements CacheInterface {
   public fork(): this {
     const Static = this.constructor as typeof Cache;
     return new Static(this) as this;
