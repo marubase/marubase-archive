439204e4f593086f014ed73164915c3f97e8f26a
Endy Jasmi <endyjasmi@gmail.com>
2022-04-05 05:08:17 +0800

    test(@marubase/foundation): update toMessage test

--- packages/foundation/source/internal/to-message.test.ts
+++ packages/foundation/source/internal/to-message.test.ts
@@ -72,4 +72,57 @@ describe("toMessage(readable)", function () {
       expect(bodyText).to.equal("hello, world");
     });
   });
+  context("when message readable have no header in the first few bytes", function () {
+    it("should throw error", async function () {
+      let index = 0;
+      const readable = new Readable({
+        read() {
+          this.push(`chunk-${index++}\r\n`);
+        },
+      });
+
+      try {
+        await toMessage(readable);
+
+        throw new Error("it should never happen");
+      } catch (error) {
+        expect(error).to.be.an.instanceOf(Error);
+      }
+    });
+  });
+  context("when message readable throw error", function () {
+    it("should throw error", async function () {
+      let rawMessage = ``;
+      rawMessage += `Content-Length: 12\r\n`;
+      rawMessage += `Content-Type: text/plain\r\n`;
+      rawMessage += `\r\n`;
+
+      let index = 0;
+      const readable = new Readable({
+        read() {
+          if (index < 1000) this.push(`chunk-${index++}\r\n`);
+          else {
+            const error = new Error("test error");
+            this.destroy(error);
+          }
+        },
+      });
+      readable.push(rawMessage);
+
+      try {
+        const { body, headers } = await toMessage(readable);
+        expect(Array.from(headers)).to.deep.equal([
+          ["Content-Length", "12"],
+          ["Content-Type", "text/plain"],
+        ]);
+
+        const bodyChunks: Buffer[] = [];
+        for await (const bodyChunk of body) bodyChunks.push(bodyChunk);
+
+        throw new Error("it should never happen");
+      } catch (error) {
+        expect(error).to.be.an.instanceOf(Error);
+      }
+    });
+  });
 });
