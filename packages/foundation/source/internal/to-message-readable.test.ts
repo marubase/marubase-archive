765f88df2376b62aa41c6b8508ce5141d0ad5b29
Endy Jasmi <endyjasmi@gmail.com>
2022-04-04 21:28:02 +0800

    test(@marubase/foundation): update toMessageReadable test

--- packages/foundation/source/internal/to-message-readable.test.ts
+++ packages/foundation/source/internal/to-message-readable.test.ts
@@ -1,19 +1,16 @@
 import { expect } from "chai";
 import { Readable } from "stream";
-import { MessageRecord, toMessageReadable } from "./to-message-readable.js";
+import { toMessageReadable } from "./to-message-readable.js";
 
 describe("toMessageReadable(message, options", function () {
   context("when message have single header", function () {
-    let message: MessageRecord;
-    beforeEach(async function () {
+    it("should return message readable", async function () {
       const headers = new Map([["Content-Type", "text/plain"]]);
       const body = new Readable();
       body.push("hello, world", "utf8");
       body.push(null);
 
-      message = { body, headers };
-    });
-    it("should return message readable", async function () {
+      const message = { body, headers };
       const messageChunks: Buffer[] = [];
       for await (const messageChunk of toMessageReadable(message)) {
         messageChunks.push(messageChunk);
@@ -30,8 +27,7 @@ describe("toMessageReadable(message, options", function () {
     });
   });
   context("when message have multiple header", function () {
-    let message: MessageRecord;
-    beforeEach(async function () {
+    it("should return message readable", async function () {
       const headers = new Map([
         ["Content-Length", "12"],
         ["Content-Type", "text/plain"],
@@ -40,9 +36,7 @@ describe("toMessageReadable(message, options", function () {
       body.push("hello, world", "utf8");
       body.push(null);
 
-      message = { body, headers };
-    });
-    it("should return message readable", async function () {
+      const message = { body, headers };
       const messageChunks: Buffer[] = [];
       for await (const messageChunk of toMessageReadable(message)) {
         messageChunks.push(messageChunk);
@@ -60,16 +54,13 @@ describe("toMessageReadable(message, options", function () {
     });
   });
   context("when message have no header", function () {
-    let message: MessageRecord;
-    beforeEach(async function () {
+    it("should return message readable", async function () {
       const headers = new Map<string, string>();
       const body = new Readable();
       body.push("hello, world", "utf8");
       body.push(null);
 
-      message = { body, headers };
-    });
-    it("should return message readable", async function () {
+      const message = { body, headers };
       const messageChunks: Buffer[] = [];
       for await (const messageChunk of toMessageReadable(message)) {
         messageChunks.push(messageChunk);
