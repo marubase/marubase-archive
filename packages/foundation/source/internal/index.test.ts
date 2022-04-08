4e266eee7cdb9aac8ddd0fee2fdfeb2db17a7a6f
Endy Jasmi <endyjasmi@gmail.com>
2022-04-08 19:32:09 +0800

    test(@marubase/foundation): update test index

--- packages/foundation/source/internal/index.test.ts
+++ packages/foundation/source/internal/index.test.ts
@@ -2,6 +2,7 @@ import "./to-buffer-readable.test.js";
 import "./to-message-readable.test.js";
 import "./to-message.test.js";
 import "./to-multipart-readable.test.js";
+import "./to-multipart.test.js";
 import "./to-request-readable.test.js";
 import "./to-request.test.js";
 import "./to-response-readable.test.js";
