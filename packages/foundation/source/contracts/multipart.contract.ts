650fb4788acb802e42b811636e2bcba5ce1f8d01
Endy Jasmi <endyjasmi@gmail.com>
2022-04-10 20:47:45 +0800

    feat(@marubase/foundation): add multipart contract

---
+++ packages/foundation/source/contracts/multipart.contract.ts
@@ -0,0 +1,20 @@
+import { Readable } from "stream";
+import { MessageInterface } from "./message.contract.js";
+
+export const MultipartContract = Symbol("MultipartContract");
+
+export interface MultipartInterface extends AsyncIterable<MessageInterface> {
+  readonly boundary: string;
+
+  readonly contentType: string;
+
+  readonly mimeType: string;
+
+  setBoundary(boundary: string): this;
+
+  setContentType(contentType: string): this;
+
+  setMimeType(mimeType: string): this;
+
+  toStream(): Readable;
+}
