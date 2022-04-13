ec42b725d9a0df2c60a322fc51431b80c4ab0fd1
Endy Jasmi <endyjasmi@gmail.com>
2022-04-13 19:17:37 +0800

    refactor(@marubase/router): rename FoundationError to RouterError

--- packages/router/source/errors/router.error.ts
+++ packages/router/source/errors/router.error.ts
@@ -1,6 +1,6 @@
 import { ResponseCode } from "../contracts/response.contract.js";
 
-export class FoundationError extends Error {
+export class RouterError extends Error {
   public constructor(message?: string, public code: ResponseCode = 500) {
     super(message);
     this.name = this.constructor.name;
