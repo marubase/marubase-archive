7d96f9a20fc35537192f197de74333c18691c95b
Endy Jasmi <endyjasmi@gmail.com>
2022-04-13 21:02:58 +0800

    feat(@marubase/router): add router provider

---
+++ packages/router/source/router.provider.ts
@@ -0,0 +1,13 @@
+import { ContainerInterface, ContainerProvider } from "@marubase/container";
+
+export class RouterProvider implements ContainerProvider {
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  public install(container: ContainerInterface): void {
+    /* eslint-disable-line @typescript-eslint/no-empty-function */
+  }
+
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  public uninstall(container: ContainerInterface): void {
+    /* eslint-disable-line @typescript-eslint/no-empty-function */
+  }
+}
