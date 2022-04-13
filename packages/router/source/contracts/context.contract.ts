2b0a5cfd5801b2991264645cbcd72d88db8c9edd
Endy Jasmi <endyjasmi@gmail.com>
2022-04-13 20:57:45 +0800

    refactor(@marubase/router): update context contract

--- packages/router/source/contracts/context.contract.ts
+++ packages/router/source/contracts/context.contract.ts
@@ -1,20 +1,13 @@
-import { RegistryTag, Resolvable } from "@marubase/container";
-import { Readable } from "stream";
-import { ManagerInterface } from "./manager.contract.js";
-import {
-  MessageBuffer,
-  MessageData,
-  MessageInterface,
-  MessageProtocol,
-} from "./message.contract.js";
-import { MultipartInterface } from "./multipart.contract.js";
+import { ContainerInterface } from "@marubase/container";
+import { MessageProtocol } from "./message.contract.js";
 import { RequestInterface, RequestMethod } from "./request.contract.js";
 import { ResponseCode, ResponseInterface } from "./response.contract.js";
-import { ServiceInterface } from "./service.contract.js";
 
 export const ContextContract = Symbol("ContextContract");
 
 export interface ContextInterface extends Map<string | symbol, unknown> {
+  readonly container: ContainerInterface;
+
   readonly credential?: [string, string] | string;
 
   readonly hash: string;
@@ -22,8 +15,6 @@ export interface ContextInterface extends Map<string | symbol, unknown> {
   readonly hostname: string;
 
   readonly href: string;
-
-  readonly manager: ManagerInterface;
 
   readonly method: RequestMethod;
 
@@ -43,25 +34,5 @@ export interface ContextInterface extends Map<string | symbol, unknown> {
 
   readonly url: URL;
 
-  call<Result>(targetFn: Function, ...args: unknown[]): Result;
-
-  create<Result>(targetClass: Function, ...args: unknown[]): Result;
-
-  factory<Result>(resolvable: Resolvable): (...args: unknown[]) => Result;
-
-  message(buffer: MessageBuffer): MessageInterface;
-  message(data: MessageData): MessageInterface;
-  message(multipart: MultipartInterface): MessageInterface;
-  message(readable: Readable): MessageInterface;
-
-  multipart(...messages: MessageInterface[]): MultipartInterface;
-
-  resolve<Result>(resolvable: Resolvable, ...args: unknown[]): Result;
-
-  resolveTag<Result>(tag: RegistryTag, ...args: unknown[]): Result[];
-
   respondWith(statusCode: ResponseCode, statusText?: string): ResponseInterface;
-
-  service(href: string, base?: string): ServiceInterface;
-  service(url: URL): ServiceInterface;
 }
