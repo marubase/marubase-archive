3fb95843dabaec0cf8f7f119d859863df5c713b8
Endy Jasmi <endyjasmi@gmail.com>
2022-04-16 19:48:15 +0800

    refactor(@marubase/router): update router provider

--- packages/router/source/router.provider.ts
+++ packages/router/source/router.provider.ts
@@ -1,13 +1,17 @@
 import { ContainerInterface, ContainerProvider } from "@marubase/container";
+import { MessageContract } from "./contracts/message.contract.js";
 import { MultipartContract } from "./contracts/multipart.contract.js";
+import { Message } from "./message.js";
 import { Multipart } from "./multipart.js";
 
 export class RouterProvider implements ContainerProvider {
   public install(container: ContainerInterface): void {
+    container.bind(MessageContract).to(Message);
     container.bind(MultipartContract).to(Multipart);
   }
 
   public uninstall(container: ContainerInterface): void {
+    container.resolver(MessageContract)?.clearRegistryKey();
     container.resolver(MultipartContract)?.clearRegistryKey();
   }
 }
