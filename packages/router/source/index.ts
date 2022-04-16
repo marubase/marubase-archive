a39c762e8434a08e0abb68affbdee501f586114f
Endy Jasmi <endyjasmi@gmail.com>
2022-04-16 19:48:01 +0800

    refactor(@marubase/router): update package index

--- packages/router/source/index.ts
+++ packages/router/source/index.ts
@@ -1,5 +1,6 @@
 export * from "@marubase/container";
 export * from "./contracts/index.js";
 export * from "./functions/index.js";
+export * from "./message.js";
 export * from "./multipart.js";
 export * from "./router.provider.js";
